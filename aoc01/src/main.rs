38d420af44f141cef275de78eda98bd6a5d46e11
modulitos <modulitos@users.noreply.github.com>
2020-01-26 18:15:48 -0800

    refactor aco1 part 1

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -14,16 +14,21 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+// Return a sum of the numbers.
 fn part1(input: &str) -> Result<i32> {
     // how to prevent integer overflow when summing?
 
-    // Can we unwrap in a way that surfaces this error, without having to resort to a for-loop?
-    // https://stackoverflow.com/a/26370894/1884158
+    // Can we unwrap in a way that surfaces this error, without having to resort to a for-loop? It
+    // would be ideal to use the '?' operator instead of expect to unwrap the result from
+    // item.parse: https://stackoverflow.com/a/26370894/1884158
 
-    Ok(input.lines().map(|item| item.parse::<i32>().unwrap()).sum())
+    Ok(input.lines().map(|item| item.parse::<i32>().expect("Unable to parse item into an i32")).sum())
 }
 
-fn part2(input: &str) {}
+//
+fn part2(input: &str) {
+
+}
 
 #[test]
 fn test_part1() -> Result<()> {
