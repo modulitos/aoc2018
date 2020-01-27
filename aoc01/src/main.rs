0e040de2c090c6092ca1d46da0c529cda2961343
modulitos <modulitos@users.noreply.github.com>
2020-01-26 21:30:00 -0800

    refactor aoc1 part2

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 use std::io::{self, Lines, Read, Write};
+use std::iter::Map;
 
 type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
@@ -18,41 +19,48 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-// Return a sum of the numbers.
-
-fn part1(input: &str) -> Result<i32> {
+fn get_nums(input: &str) -> Map<std::str::Lines, fn(&str) -> i32> {
     // TODO: Can we unwrap in a way that surfaces this error, without having to resort to a for-loop? It
     // would be ideal to use the '?' operator instead of expect to unwrap the result from
     // item.parse: https://stackoverflow.com/a/26370894/1884158
 
-    Ok(input
-        .lines()
-        .map(|item| {
-            item.parse::<i32>()
-                .expect("Unable to parse item into an i32")
-        })
-        // TODO: how to prevent integer overflow when summing?
-        .sum())
+    input.lines().map(|item| {
+        item.parse::<i32>()
+            .expect("Unable to parse item into an i32")
+    })
+}
+
+// Return a sum of the numbers.
+
+fn part1(input: &str) -> Result<i32> {
+    // TODO: how to prevent integer overflow when summing?
+    Ok(get_nums(input).sum())
 }
 
 // Find the value of the first ongoing sum that repeats twice, and looping through the nums if
 // necessary.
 
 fn part2(input: &str) -> Result<i32> {
-    let mut freqs: HashSet<i32> = HashSet::new();
+    let mut seen: HashSet<i32> = HashSet::new();
 
     let mut freq = 0;
-    freqs.insert(freq);
+    seen.insert(freq);
 
-    // TODO: Is there a way to do this without a for loop?
+    // TODO: Is there a way to do this without a loop?
     loop {
-        for line in input.lines() {
-            freq += line.parse::<i32>()?;
-            if freqs.contains(&freq) {
-                return Ok(freq);
-            } else {
-                freqs.insert(freq);
-            }
+        if get_nums(input)
+            .find(|num| {
+                freq += *num;
+                if seen.contains(&freq) {
+                    true
+                } else {
+                    seen.insert(freq);
+                    false
+                }
+            })
+            .is_some()
+        {
+            return Ok(freq);
         }
     }
 }
