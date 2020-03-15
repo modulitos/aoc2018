326888de842a990657039b591fe6d2f0a88a0f78
modulitos <modulitos@users.noreply.github.com>
2020-03-15 13:07:46 -0700

    aoc13 part 2

--- aoc13/src/main.rs
+++ aoc13/src/main.rs
@@ -1,5 +1,5 @@
 use std::boxed;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashSet};
 use std::error;
 use std::io::{Read, Write};
 use std::ops::{Add, Sub};
@@ -14,11 +14,17 @@ fn main() -> Result<()> {
     std::io::stdin().read_to_string(&mut input)?;
 
     let sim = input.parse::<Simulation>()?;
-    let coord = sim.run_to_first_collision()?;
     writeln!(
         std::io::stdout(),
-        "coordinate of first collision: {:?}",
-        coord
+        "coordinate of collision: {:?}",
+        sim.get_first_collision()?
+    )?;
+
+    let sim = input.parse::<Simulation>()?;
+    writeln!(
+        std::io::stdout(),
+        "coordinate of last cart: {:?}",
+        sim.get_last_cart()?
     )?;
     Ok(())
 }
@@ -34,61 +40,62 @@ enum Track {
     CurveBackward, // back slash: \
 }
 
+enum SimulationResult {
+    Collision(Coordinate),
+    LastCart(Coordinate), // returns coord of last cart, if there is one
+    Step,
+}
+
 struct Simulation {
     track: Vec<Vec<Track>>,
     carts: BTreeMap<Coordinate, Cart>,
 }
 
-enum SimulationResult {
-    Collision(Coordinate),
-    LastCart(Coordinate),
-    Step,
-}
-
 impl Simulation {
-
-    fn run_to_first_collision(mut self) -> Result<Coordinate> {
-        loop {
-            if let SimulationResult::Collision(coord) = self.process_tick()? {
-                return Ok(coord);
-            }
+    fn get_first_collision(self) -> Result<Coordinate> {
+        if let Some(coord) = self
+            .into_iter()
+            .collect::<Result<Vec<SimulationResult>>>()?
+            .into_iter()
+            .find_map(|result| {
+                if let SimulationResult::Collision(coord) = result {
+                    Some(coord)
+                } else {
+                    None
+                }
+            })
+        {
+            Ok(coord)
+        } else {
+            Err(Error::from("no collision found!"))
         }
     }
 
-    // Cycle through all carts once. If two carts collide, return the coord of the collision and
-    // remove the carts from the grid.
-
-    // Returns an error if there is an invariant violated on the track.
-
-    fn process_tick(&mut self) -> Result<SimulationResult> {
-        let previous_carts = std::mem::replace(&mut self.carts, BTreeMap::new());
-        let mut iter = previous_carts.into_iter();
-
-        while let Some((mut coord, mut cart)) = iter.next() {
-            // Check whether any carts are in the current coordinate:
-            if self.carts.contains_key(&coord) {
-                // Note: instead of short-circuiting, we can also mark the cart as crashed here:
-                return Ok(SimulationResult::Collision(coord));
-            }
-
-            coord.update_from_cart_direction(&cart.direction)?;
-
-            // Check whether any carts are in the new coordinate:
-            if self.carts.contains_key(&coord) {
-                // Note: instead of short-circuiting, we can also mark the cart as crashed here:
-                return Ok(SimulationResult::Collision(coord));
-            }
-
-            // update the cart's direction based on the new coordinate's track:
-            let new_track = self.track[coord.y as usize][coord.x as usize];
-            if let Err(error) = cart.update_from_track(&new_track) {
-                // Pass along the error, but adding some extra context about the coordinate:
-                return Err(Error::from(format!("{} at: {:?}", error, coord)));
-            }
-
-            self.carts.insert(coord, cart);
+    fn get_last_cart(self) -> Result<Coordinate> {
+        if let Some(coord) = self
+            .into_iter()
+            .collect::<Result<Vec<SimulationResult>>>()?
+            .into_iter()
+            .find_map(|result| {
+                if let SimulationResult::LastCart(coord) = result {
+                    Some(coord)
+                } else {
+                    None
+                }
+            })
+        {
+            Ok(coord)
+        } else {
+            Err(Error::from("no last cart found!"))
         }
-        Ok(SimulationResult::Step)
+    }
+
+    fn into_iter(self) -> SimulationIter {
+        SimulationIter {
+            track: self.track,
+            carts: self.carts,
+            error_found: false,
+        }
     }
 }
 
@@ -140,7 +147,91 @@ impl FromStr for Simulation {
     }
 }
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Clone, Debug)]
+struct SimulationIter {
+    track: Vec<Vec<Track>>,
+    carts: BTreeMap<Coordinate, Cart>,
+    // This is for easier error handling within the iterator:
+    // https://users.rust-lang.org/t/handling-errors-from-iterators/2551/14
+    // TODO: But maybe a loop would've been better than an iterator here, to avoid nesting Option<Result<...>>?
+    // OR maybe it's better to panic than return Err's from the iterator?
+    error_found: bool,
+}
+
+impl Iterator for SimulationIter {
+    type Item = Result<SimulationResult>;
+
+    // Cycle through all carts once. If two carts collide, return the coord of the collision and
+    // remove the carts from the grid. Return the coord of the last remaining cart, if there is one.
+
+    // Returns an error if there is an invariant violated on the track.
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.carts.len() == 0 || self.error_found {
+            return None;
+        } else if self.carts.len() == 1 {
+            // Base case: there are 1 or 0 carts left.
+            let coord = self.carts.keys().cloned().last().unwrap();
+            self.carts.clear();
+            return Some(Ok(SimulationResult::LastCart(coord)));
+        }
+        let previous_carts = std::mem::replace(&mut self.carts, BTreeMap::new());
+        let mut previous_cart_coords = previous_carts
+            .keys()
+            .cloned()
+            .collect::<HashSet<Coordinate>>();
+        let mut crash_coords = HashSet::new();
+
+        let mut first_collision_coord = None;
+        let mut iter = previous_carts.into_iter();
+        while let Some((mut coord, mut cart)) = iter.next() {
+            if crash_coords.contains(&coord) {
+
+                // another cart has run into this cart on a previous round, but the cart wasn't yet
+                // removed
+
+                continue;
+            }
+
+            previous_cart_coords.remove(&coord);
+
+            if let Err(error) = coord.update_from_cart_direction(&cart.direction) {
+                self.error_found = true;
+                return Some(Err(error));
+            }
+
+            // update the cart's direction based on the new coordinate's track:
+            let new_track = self.track[coord.y as usize][coord.x as usize];
+            if let Err(error) = cart.update_from_track(&new_track) {
+                self.error_found = true;
+                // Pass along the error, but adding some extra context about the coordinate:
+                return Some(Err(Error::from(format!("{} at: {:?}", error, coord))));
+            }
+
+            // Check whether any carts are in the new coordinate:
+            if crash_coords.contains(&coord)
+                || self.carts.contains_key(&coord)
+                || previous_cart_coords.contains(&coord)
+            {
+                crash_coords.insert(coord);
+                // remove the crashed cart, and don't add this cart to our collection.
+                self.carts.remove(&coord);
+                if first_collision_coord.is_none() {
+                    first_collision_coord = Some(coord);
+                }
+            } else {
+                // There was no collision, so update the cart with its new location:
+                self.carts.insert(coord.clone(), cart);
+            }
+        }
+        if let Some(coord) = first_collision_coord {
+            Some(Ok(SimulationResult::Collision(coord)))
+        } else {
+            Some(Ok(SimulationResult::Step))
+        }
+    }
+}
+
+#[derive(Ord, PartialOrd, Eq, PartialEq, Clone, Copy, Debug, Hash)]
 struct Coordinate {
     y: u32,
     x: u32,
@@ -243,7 +334,7 @@ impl Cart {
             0 => direction + 1, // turn right
             1 => direction - 1, // turn left
             2 => direction,     // go straight
-            _ => panic!("unreachable code for self.turns: {}", self.turns)
+            _ => panic!("unreachable code for self.turns: {}", self.turns),
         }
     }
 
@@ -301,15 +392,29 @@ fn test_first_crash_detection() -> Result<()> {
     println!("s: \n{}", s);
     let sim = s.parse::<Simulation>()?;
 
-    assert_eq!(sim.run_to_first_collision()?, Coordinate { x: 7, y: 3 });
+    println!("getting first collision...");
+    assert_eq!(sim.get_first_collision()?, Coordinate { x: 7, y: 3 });
 
-    println!("track parsing passed!");
+    println!("test_first_crash_detection passed!");
     Ok(())
 }
 
-
 #[test]
 fn test_last_cart() -> Result<()> {
+    let s = r"/>-<\
+|   |
+| /<+-\
+| | | v
+\>+</ |
+  |   ^
+  \<->/";
+    println!("s: \n{}", s);
+    let sim = s.parse::<Simulation>()?;
+
+    println!("testing last_cart...");
+    assert_eq!(sim.get_last_cart()?, Coordinate { x: 6, y: 4 });
+
+    println!("test_last_cart passed!");
     Ok(())
 }
 
@@ -346,4 +451,5 @@ fn test_direction_arithmetic() {
     assert_eq!(Left - 2, Right);
     assert_eq!(Left - 4, Left);
     assert_eq!(Left + 4, Left);
+    println!("test direction arithmetic passed!");
 }
