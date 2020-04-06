590eb91d1205e8a6294c4eda08cecafde7db0c40
modulitos <modulitos@users.noreply.github.com>
2020-04-05 22:51:05 -0700

    aoc19 parts 1 and 2

--- aoc19/src/main.rs
+++ aoc19/src/main.rs
@@ -1,12 +1,194 @@
 mod error;
 mod op_codes;
+use std::result::Result::Err;
 
 use error::{Error, Result};
-use op_codes::{Opcode, OpcodeName, Registers};
+use op_codes::{Op, Opcode, OpcodeId, RegisterId, Registers};
+use std::fs::{canonicalize, File};
+use std::io::{BufRead, BufReader, Read, Write};
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input)?;
+
+    let mut cpu = input.parse::<CPU>()?;
+
+    writeln!(
+        std::io::stdout(),
+        "value of Register 0 when halted: {}",
+        cpu.run()
+    )?;
+
+    let mut cpu_2 = input.parse::<CPU>()?;
+
+    cpu_2.registers.set(RegisterId::R0, 1);
+    writeln!(
+        std::io::stdout(),
+        "when started with a value of 1 in R0, the value of Register 0 when halted is: {}",
+        cpu_2.run()
+    )?;
+
     Ok(())
+}
+
+struct CPU {
+    registers: Registers,
+    ops: Vec<Op>,
+    ip_register: RegisterId,
+}
+
+impl CPU {
+    // Opitimize the assembly code. This was specific to my input, so every input will vary. You'll
+    // need to examine your assembly code for sequences of opcodes that are looping exccessively,
+    // and find a way to optimize them.
+
+    fn step_fast(&mut self) {
+        // the goal of the IP=3 loop is to find the factor equal to R4 / R1, and greater than or
+        // equal to the value of R3.
+        //
+        // Once found, set that value to R3 and goto IP=7, and set R5 to 1.
+
+        // If no factor can be found to match that criteria, then set R3 to be (R4 + 1), and goto IP
+        // = 12, and set R5 to 0.
+
+        // Here is the logic in the opcodes, which is doing this calculation extremely inefficiently:
+        /*
+        R5 = R1 * R3
+        # when R2  pi=4
+        if R4 == R5:
+          R5 = 1
+          R2 += 1 (R5)
+          # goto IP=7
+        else:
+          R5 = 0
+          # continue to IP=6
+
+        # IP=6
+        R2 += 1
+
+        # IP=8
+        R3 += 1
+
+        # IP=9
+        if R3 > R4:
+          R5 = 1
+          R2 += 1 (R5)
+          # goto IP=12
+        else:
+          R5 = 0
+          # continue to IP=10
+          # IP=10
+          R2 += R5
+          # goto ip=3
+        */
+
+        // which can also be translated to this:
+        /*
+        loop {
+            if r3 * r1 == r4 {
+                let r0 = self.registers.get(RegisterId::R0);
+                self.registers.set(RegisterId::R0, r0 + r1);
+            }
+
+            r3 += 1;
+
+            if r3 > r4 {
+                new_r3 = r3;
+                break;
+            }
+        }
+        */
+
+        // and the loop above can be further optimized like so:
+
+        let r4 = self.registers.get(RegisterId::R4);
+        let r1 = self.registers.get(RegisterId::R1);
+        let r3 = self.registers.get(RegisterId::R3);
+
+        if r4 % r1 == 0 && r3 <= (r4 / r1) {
+            // if a factor is possible, and we haven't gone passed it:
+            let r0 = self.registers.get(RegisterId::R0);
+            self.registers.set(RegisterId::R0, r0 + r1);
+        }
+
+        self.registers.set(RegisterId::R5, 1);
+        self.registers.set(RegisterId::R3, r4 + 1);
+        self.registers.set(self.ip_register, 12);
+    }
+
+    // runs the Op at the current instruction pointer (IP), then increments the IP.
+    //
+    // If the IP is outside the range of our program, we halt, and return the invalid IP upon
+    // halting.
+
+    fn step(&mut self) -> Result<(), u32> {
+        // TODO: ideally, we can update our IP RegisterId's type to be a usize...
+        let ip = self.registers.get(self.ip_register) as usize;
+
+        // Optimization for solving part 2 - when IP=3, skip the opcodes and execute an optimized
+        // form instead.
+
+        if ip == 3 {
+            self.step_fast();
+            return Ok(());
+        }
+
+        let op = self
+            .ops
+            .get(ip)
+            .expect("IP should not point outside the program instruction range.");
+        let mut next_registers = op.exec(&self.registers);
+        let next_ip = next_registers.get(self.ip_register) + 1;
+
+        next_registers.set(self.ip_register, next_ip);
+
+        self.registers = next_registers;
+        if next_ip < (self.ops.len() as u32) {
+            Ok(())
+        } else {
+            // Stop the program once the IP goes out of range, returning the value in R0:
+            Err(self.registers.get(RegisterId::R0))
+        }
+    }
+
+    // steps through our program until it halts, returning value at R0.
+
+    fn run(&mut self) -> u32 {
+        loop {
+            match self.step() {
+                Ok(()) => continue,
+                Err(r0_val) => return r0_val,
+            }
+        }
+    }
+}
+
+impl FromStr for CPU {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+        let ip_register = match lines.next() {
+            Some(line) => line.trim_start_matches("#ip ").parse::<RegisterId>()?,
+            None => {
+                return Err(Self::Err::from(format!(
+                    "unable to parse first string from lines: {:?}",
+                    lines
+                )))
+            }
+        };
+        let ops = lines
+            .map(|line| line.parse::<Op>())
+            .collect::<Result<Vec<Op>>>()?;
+        let registers = Registers([0; 6]);
+        Ok(Self {
+            registers,
+            ops,
+            ip_register: ip_register,
+        })
+    }
 }
 
 #[test]
@@ -21,6 +203,61 @@ fn test_instruction_pointer() -> Result<()> {
         seti 8 0 4\n\
         seti 9 0 5\n\
     ";
+    let mut cpu = input.parse::<CPU>()?;
+
+    assert!(cpu.step().is_ok());
+    assert_eq!(cpu.registers, Registers([1, 5, 0, 0, 0, 0]));
+    assert!(cpu.step().is_ok());
+    assert_eq!(cpu.registers, Registers([2, 5, 6, 0, 0, 0]));
+    assert!(cpu.step().is_ok());
+    assert_eq!(cpu.registers, Registers([4, 5, 6, 0, 0, 0]));
+    assert!(cpu.step().is_ok());
+    assert_eq!(cpu.registers, Registers([6, 5, 6, 0, 0, 0]));
+
+    assert_eq!(cpu.run(), 7);
+
     println!("test_instruction_pointer passed.");
+
+    Ok(())
+}
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+#[test]
+fn test_part_1() -> Result<()> {
+    let file_name = PathBuf::from("./input/input.txt");
+    println!("file_name: {:?}", file_name);
+    // gets the file path relative to the cargo project dir
+    let file_path = canonicalize(&file_name)?;
+    println!("file_path: {:?}", file_path);
+    let input = &lines_from_file(file_path).join("\n");
+
+    let mut cpu = input.parse::<CPU>()?;
+
+    assert_eq!(cpu.run(), 1248);
+    println!("test_part_1 passed.");
+    Ok(())
+}
+
+#[test]
+fn test_part_2() -> Result<()> {
+    let file_name = PathBuf::from("./input/input.txt");
+    println!("file_name: {:?}", file_name);
+    // gets the file path relative to the cargo project dir
+    let file_path = canonicalize(&file_name)?;
+    println!("file_path: {:?}", file_path);
+    let input = &lines_from_file(file_path).join("\n");
+
+    let mut cpu = input.parse::<CPU>()?;
+    cpu.registers.set(RegisterId::R0, 1);
+
+    assert_eq!(cpu.run(), 14952912);
+    println!("test_part_2 passed.");
     Ok(())
 }
--- aoc19/src/op_codes.rs
+++ aoc19/src/op_codes.rs
@@ -1,16 +1,20 @@
 use crate::{Error, Result};
 use std::convert::{From, TryFrom};
 use std::slice::Iter;
+use std::str::FromStr;
 
 // This module contains all logic pertaining to our registers and opcodes.
 
 #[derive(Copy, Clone, Debug)]
 // This is just a ranged type.
-enum RegisterId {
+// TODO: is there a better way to implement a ranged type?
+pub enum RegisterId {
     R0,
     R1,
     R2,
     R3,
+    R4,
+    R5,
 }
 
 impl RegisterId {
@@ -20,38 +24,65 @@ impl RegisterId {
             1 => Ok(RegisterId::R1),
             2 => Ok(RegisterId::R2),
             3 => Ok(RegisterId::R3),
+            4 => Ok(RegisterId::R4),
+            5 => Ok(RegisterId::R5),
             _ => Err(Error::from(format!("must be within [0-3]: {}", n))),
         }
+    }
+}
+
+impl FromStr for RegisterId {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.parse::<u8>()? {
+            0 => RegisterId::R0,
+            1 => RegisterId::R1,
+            2 => RegisterId::R2,
+            3 => RegisterId::R3,
+            4 => RegisterId::R4,
+            5 => RegisterId::R5,
+            _ => {
+                return Err(Self::Err::from(format!(
+                    "cannot parse RegisterId from string: {}",
+                    s
+                )))
+            }
+        })
     }
 }
 
 type RegisterValue = u32;
 
 #[derive(Clone, Eq, PartialEq, Debug)]
-pub struct Registers(pub [RegisterValue; 4]);
+pub struct Registers(pub [RegisterValue; 6]);
 
 impl Registers {
-    fn get(&self, id: RegisterId) -> RegisterValue {
+    pub fn get(&self, id: RegisterId) -> RegisterValue {
         match id {
             RegisterId::R0 => self.0[0],
             RegisterId::R1 => self.0[1],
             RegisterId::R2 => self.0[2],
             RegisterId::R3 => self.0[3],
+            RegisterId::R4 => self.0[4],
+            RegisterId::R5 => self.0[5],
         }
     }
-    fn set(&mut self, id: RegisterId, value: RegisterValue) {
+    pub fn set(&mut self, id: RegisterId, value: RegisterValue) {
         let register = match id {
             RegisterId::R0 => &mut self.0[0],
             RegisterId::R1 => &mut self.0[1],
             RegisterId::R2 => &mut self.0[2],
             RegisterId::R3 => &mut self.0[3],
+            RegisterId::R4 => &mut self.0[4],
+            RegisterId::R5 => &mut self.0[5],
         };
         *register = value;
     }
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
-pub enum OpcodeName {
+pub enum OpcodeId {
     Addr,
     Addi,
     Mulr,
@@ -70,10 +101,10 @@ pub enum OpcodeName {
     Eqrr,
 }
 
-impl OpcodeName {
-    pub fn iter() -> Iter<'static, OpcodeName> {
-        use OpcodeName::*;
-        static IDS: [OpcodeName; 16] = [
+impl OpcodeId {
+    pub fn iter() -> Iter<'static, OpcodeId> {
+        use OpcodeId::*;
+        static IDS: [OpcodeId; 16] = [
             Addr, Addi, Mulr, Muli, Banr, Bani, Borr, Bori, Setr, Seti, Gtir, Gtri, Gtrr, Eqir,
             Eqri, Eqrr,
         ];
@@ -81,96 +112,124 @@ impl OpcodeName {
     }
 }
 
-pub struct Opcode {
-    pub id: OpcodeName,
-    kind: Op,
-    c: RegisterId, // the register that will take the output of the opcode
+impl FromStr for OpcodeId {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use OpcodeId::*;
+        let name = match s {
+            "addr" => Addr,
+            "addi" => Addi,
+            "mulr" => Mulr,
+            "muli" => Muli,
+            "banr" => Banr,
+            "bani" => Bani,
+            "borr" => Borr,
+            "bori" => Bori,
+            "setr" => Setr,
+            "seti" => Seti,
+            "gtir" => Gtir,
+            "gtri" => Gtri,
+            "gtrr" => Gtrr,
+            "eqir" => Eqir,
+            "eqri" => Eqri,
+            "eqrr" => Eqrr,
+            _ => return Err(Self::Err::from(format!("could not convert from {}", s))),
+        };
+        Ok(name)
+    }
 }
 
-impl Opcode {
+#[derive(Debug)]
+pub struct Op {
+    opcode: Opcode,
+    output: RegisterId, // the register that will take the output of the opcode
+}
+
+impl Op {
     // Get the opcode corresponding to the provided OpcodeName, using the values from the
     // instruction set
 
-    pub fn from_args(name: OpcodeName, a: u8, b: u8, c: u8) -> Result<Self> {
+    pub fn from_args(name: OpcodeId, a: u8, b: u8, c: u8) -> Result<Self> {
         let a = a;
         let b = b;
         let c = c;
 
-        use Op::*;
+        use Opcode::*;
         let mkid = RegisterId::from_number;
         let mkval = RegisterValue::try_from;
 
         let kind = match name {
-            OpcodeName::Addr => Addr {
+            OpcodeId::Addr => Addr {
                 a: mkid(a)?,
                 b: mkid(b)?,
             },
-            OpcodeName::Addi => Addi {
+            OpcodeId::Addi => Addi {
                 a: mkid(a)?,
                 b: mkval(b)?,
             },
-            OpcodeName::Mulr => Mulr {
+            OpcodeId::Mulr => Mulr {
                 a: mkid(a)?,
                 b: mkid(b)?,
             },
-            OpcodeName::Muli => Muli {
+            OpcodeId::Muli => Muli {
                 a: mkid(a)?,
                 b: mkval(b)?,
             },
-            OpcodeName::Banr => Banr {
+            OpcodeId::Banr => Banr {
                 a: mkid(a)?,
                 b: mkid(b)?,
             },
-            OpcodeName::Bani => Bani {
+            OpcodeId::Bani => Bani {
                 a: mkid(a)?,
                 b: mkval(b)?,
             },
-            OpcodeName::Borr => Borr {
+            OpcodeId::Borr => Borr {
                 a: mkid(a)?,
                 b: mkid(b)?,
             },
-            OpcodeName::Bori => Bori {
+            OpcodeId::Bori => Bori {
                 a: mkid(a)?,
                 b: mkval(b)?,
             },
-            OpcodeName::Setr => Setr { a: mkid(a)? },
-            OpcodeName::Seti => Seti { a: mkval(a)? },
-            OpcodeName::Gtir => Gtir {
+            OpcodeId::Setr => Setr { a: mkid(a)? },
+            OpcodeId::Seti => Seti { a: mkval(a)? },
+            OpcodeId::Gtir => Gtir {
                 a: mkval(a)?,
                 b: mkid(b)?,
             },
-            OpcodeName::Gtri => Gtri {
+            OpcodeId::Gtri => Gtri {
                 a: mkid(a)?,
                 b: mkval(b)?,
             },
-            OpcodeName::Gtrr => Gtrr {
+            OpcodeId::Gtrr => Gtrr {
                 a: mkid(a)?,
                 b: mkid(b)?,
             },
-            OpcodeName::Eqir => Eqir {
+            OpcodeId::Eqir => Eqir {
                 a: mkval(a)?,
                 b: mkid(b)?,
             },
-            OpcodeName::Eqri => Eqri {
+            OpcodeId::Eqri => Eqri {
                 a: mkid(a)?,
                 b: mkval(b)?,
             },
-            OpcodeName::Eqrr => Eqrr {
+            OpcodeId::Eqrr => Eqrr {
                 a: mkid(a)?,
                 b: mkid(b)?,
             },
         };
-        Ok(Opcode {
-            id: name,
-            kind,
-            c: mkid(c)?,
+        Ok(Op {
+            // id: name,
+            opcode: kind,
+            output: mkid(c)?,
         })
     }
 
     pub fn exec(&self, registers: &Registers) -> Registers {
         let mut result = registers.clone();
-        use Op::*;
-        let new_val = match &self.kind {
+        use Opcode::*;
+        let new_val = match &self.opcode {
             &Addr { a, b } => result.get(a) + result.get(b),
             &Addi { a, b } => result.get(a) + b,
             &Mulr { a, b } => result.get(a) * result.get(b),
@@ -224,12 +283,38 @@ impl Opcode {
                 }
             }
         };
-        result.set(self.c, new_val);
+        result.set(self.output, new_val);
         result
     }
 }
 
-enum Op {
+impl FromStr for Op {
+    type Err = Error;
+
+    // eg: seti 5 0 1
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let matches = s.split(" ").collect::<Vec<&str>>();
+        let (id, v0, v1, v2) = match matches.as_slice() {
+            [name, v0, v1, v2] => (
+                name.parse::<OpcodeId>()?,
+                v0.parse::<u8>()?,
+                v1.parse::<u8>()?,
+                v2.parse::<u8>()?,
+            ),
+            _ => {
+                return Err(Self::Err::from(format!(
+                    "cannot parse the vec into an opcode: {:?}",
+                    matches
+                )))
+            }
+        };
+        Op::from_args(id, v0, v1, v2)
+    }
+}
+
+#[derive(Debug)]
+pub enum Opcode {
     Addr { a: RegisterId, b: RegisterId },
     Addi { a: RegisterId, b: RegisterValue },
     Mulr { a: RegisterId, b: RegisterId },
@@ -248,3 +333,4 @@ enum Op {
     Eqrr { a: RegisterId, b: RegisterId },
 }
 
+// impl
